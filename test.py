8a0b78553134604e8a9efd0c87f41cc2976c8648
roshan <roshan@mamiya-its.co.jp>
2024-09-05 09:19:11 +0900

    Fixed favicon.ico

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-import warnings
-from glob import glob
-from tqdm import tqdm
-from run_whisper import inference
+transcript = ""
 
-warnings.filterwarnings("ignore")
-
-files = glob('/Users/mits-mac-001/Downloads/call_center_audios/*.mp3')
-
-output_file_path = 'inference_results.txt'
-
-with open(output_file_path, 'w') as output_file:
-    for audio in tqdm(files):
-        result = inference(audio)
-        output_file.write(f"{audio}: {result}\n")
-        print(result, audio)
+def post_process(text):
+    
+    return text
